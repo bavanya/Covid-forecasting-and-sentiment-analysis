21fc1cee99e2f66114564da547f0f1fdcc7d74a5
bavanya <kurrabchoudhry.cse18@itbhu.ac.in>
2020-09-01 00:01:08 +0530

    add pretty print logic to python file

--- timeSeriesForecast/dataCollection/fromPomber/collectData.py
+++ timeSeriesForecast/dataCollection/fromPomber/collectData.py
@@ -1,7 +1,13 @@
 import requests
 import json
+import pprint
 
 response = requests.get("https://pomber.github.io/covid19/timeseries.json")
 responseInJson = response.json()
 with open('data.json', 'w') as f:
     json.dump(responseInJson, f)
+
+with open('data.json', 'r') as p:
+    data = p.read()
+    json_data = json.loads(data)
+pprint.pprint(json_data)    
